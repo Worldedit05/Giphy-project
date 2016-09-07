d281c29f4a7a93a52917f8ff43e18e75814a69b4
Aaron Lee <worldedit05@gmail.com>
2016-09-07 01:07:05 -0500

    Added AJAX call and displayed the gifs upon button press

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 var topics = ['Auburn Tigers', 'Alabama Crimson Tide', 'LSU Tigers', 'Georgia Bulldogs', 'Tennessee Volunteers', 'Ole Miss Rebels', 'South Carolina Gamecocks', 'Arkansas Razorbacks', 'Kentucky Wildcats', 'Missouri Tigers', 'Mississippi State Bulldogs', 'Texas A&M Aggies', 'Florida Gators', 'Vanderbilt Commodores']
 
+var baseQueryURL = "http://api.giphy.com/v1/gifs/search?q=";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var limit = "&limit=10";
+var rating = "&rating=pg";
+var offset = "&offset="
+var offsetCount = 0;
+
 renderButtons();
 
 function renderButtons() {
@@ -19,3 +26,54 @@ function renderButtons() {
         $('.team-buttons').append(button);
     }
 }
+
+function queryGiphy() {
+
+  $('#gifs').empty();
+
+  var team = $(this).attr('data-team');
+  var queryURL = baseQueryURL + team + limit + rating + offset + offsetCount + apiKey;
+  console.log(queryURL);
+
+  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+
+      var teamDiv = $('<div>');
+      var p = $('<p>').text('Rating: ' + results[i].rating.toUpperCase());
+      var teamImage = $('<img>');
+
+      teamImage.attr({
+          'class': 'gif center-block',
+          'src': results[i].images.fixed_height_still.url,
+          'data-still': results[i].images.fixed_height_still.url,
+          'data-animate': results[i].images.fixed_height.url,
+          'data-state': 'still'
+      });
+
+      p.attr('class', 'text-center')
+      teamDiv.append(teamImage);
+      teamDiv.append(p);
+
+      $('#gifs').prepend(teamDiv);
+    }
+  })
+}
+
+function animateGif() {
+  var state = $(this).attr('data-state');
+
+  if ( state === 'still') {
+    $(this).attr('src', $(this).data('animate'));
+    $(this).attr('data-state','animate');
+  }else{
+    $(this).attr('src', $(this).data('still'));
+    $(this).attr('data-state', 'still');
+  }
+}
+
+$(document).on('click', '.team', queryGiphy);
+
+$(document).on('click', '.gif', animateGif);
