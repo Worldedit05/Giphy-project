3fdc15f3f9d91b7266923d1e90bb674ae1bd94f3
Aaron Lee <worldedit05@gmail.com>
2016-09-08 00:21:39 -0500

    Added the ability to push new teams to the array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ['Auburn Tigers','Texas Longhorns', 'Alabama Crimson Tide', 'LSU Tigers', 'Georgia Bulldogs', 'Tennessee Volunteers', 'Ole Miss Rebels', 'South Carolina Gamecocks', 'Arkansas Razorbacks', 'Kentucky Wildcats', 'Missouri Tigers', 'Mississippi State Bulldogs', 'Texas A&M Aggies', 'Florida Gators', 'Vanderbilt Commodores']
+var topics = ['Auburn Tigers', 'Texas Longhorns', 'Alabama Crimson Tide', 'LSU Tigers', 'Georgia Bulldogs', 'Tennessee Volunteers', 'Ole Miss Rebels', 'South Carolina Gamecocks', 'Arkansas Razorbacks', 'Kentucky Wildcats', 'Missouri Tigers', 'Mississippi State Bulldogs', 'Texas A&M Aggies', 'Florida Gators', 'Vanderbilt Commodores']
 
 var baseQueryURL = "http://api.giphy.com/v1/gifs/search?q=";
 var apiKey = "&api_key=dc6zaTOxFJmzC";
@@ -8,6 +8,21 @@ var offset = "&offset="
 var offsetCount = 0;
 
 renderButtons();
+
+$(document).on('click', '.team', queryGiphy);
+
+$(document).on('click', '.gif', animateGif);
+
+$('#submitBtn').on('click', function(event) {
+
+  event.preventDefault();
+  var newTeam = $('#teamInput').val().trim();
+
+  topics.push(newTeam);
+
+  renderButtons();
+
+})
 
 function renderButtons() {
 
@@ -29,52 +44,51 @@ function renderButtons() {
 
 function queryGiphy() {
 
-  $('.row').removeClass('hidden');
-  $('#gifs').empty();
+    $('.row').removeClass('hidden');
+    $('#gifs').empty();
 
-  var team = $(this).attr('data-team');
-  var queryURL = baseQueryURL + team + limit + rating + offset + offsetCount + apiKey;
-  console.log(queryURL);
+    var team = $(this).attr('data-team');
+    var queryURL = baseQueryURL + team + limit + rating + offset + offsetCount + apiKey;
+    console.log(queryURL);
 
-  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response) {
 
-    var results = response.data;
+        var results = response.data;
 
-    for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
 
-      var teamDiv = $('<div>');
-      var p = $('<p>').text('Rating: ' + results[i].rating.toUpperCase());
-      var teamImage = $('<img>');
+            var teamDiv = $('<div>');
+            var p = $('<p>').text('Rating: ' + results[i].rating.toUpperCase());
+            var teamImage = $('<img>');
 
-      teamImage.attr({
-          'class': 'gif center-block',
-          'src': results[i].images.fixed_height_still.url,
-          'data-still': results[i].images.fixed_height_still.url,
-          'data-animate': results[i].images.fixed_height.url,
-          'data-state': 'still'
-      });
+            teamImage.attr({
+                'class': 'gif center-block',
+                'src': results[i].images.fixed_height_still.url,
+                'data-still': results[i].images.fixed_height_still.url,
+                'data-animate': results[i].images.fixed_height.url,
+                'data-state': 'still'
+            });
 
-      p.attr('class', 'text-center')
-      teamDiv.append(teamImage);
-      teamDiv.append(p);
+            p.attr('class', 'text-center')
+            teamDiv.append(teamImage);
+            teamDiv.append(p);
 
-      $('#gifs').prepend(teamDiv);
-    }
-  })
+            $('#gifs').prepend(teamDiv);
+        }
+    })
 }
 
 function animateGif() {
-  var state = $(this).attr('data-state');
+    var state = $(this).attr('data-state');
 
-  if ( state === 'still') {
-    $(this).attr('src', $(this).data('animate'));
-    $(this).attr('data-state','animate');
-  }else{
-    $(this).attr('src', $(this).data('still'));
-    $(this).attr('data-state', 'still');
-  }
+    if (state === 'still') {
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
 }
-
-$(document).on('click', '.team', queryGiphy);
-
-$(document).on('click', '.gif', animateGif);
